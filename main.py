ccd8208f91441ac07bf694711eb043e79b366ac3
banna12345bob <59811283+banna12345bob@users.noreply.github.com>
2022-10-13 14:00:27 +1000

    Acceleration works

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import radio
 radio.on()
 while True:
     try:
-        radio.send_bytes(str(accelerometer.get_values()))
+        radio.send_bytes("x"+str(accelerometer.get_x()))
+        radio.send_bytes("y"+str(accelerometer.get_y()))
+        display.show(Image.ANGRY)
     except:
+        display.scroll("FAIL")
         radio.send_bytes("FAIL")
-    display.show(Image.ANGRY)
