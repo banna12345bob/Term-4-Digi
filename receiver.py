267a2f8401f5bddbc6ae84140e66bb1928bd870f
banna12345bob <59811283+banna12345bob@users.noreply.github.com>
2022-10-17 13:56:44 +1000

    Pygame testing

--- receiver.py
+++ receiver.py
@@ -1,9 +1,14 @@
 from bitio.src import microbit
 import math
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode()
 
 microbit.radio.on()
 x, y = 0, 0
-while True:
+running = True
+while running:
     incoming = microbit.radio.receive_bytes()
     incoming = incoming[2:-1]
     if incoming[0] == "x":
@@ -17,3 +22,13 @@ while True:
     acceleration = math.sqrt(pow(x, 2) + pow(y, 2))
     if (acceleration-1000.0)>0:
         print(acceleration-1000.0)
+
+    # Pygame stuff
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill((255, 255, 255))
+    pygame.draw.circle(screen, (0,0,255), (250, 250), 75)
+    pygame.display.flip()
+
+pygame.quit()
