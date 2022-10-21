aff27580b605c5c31c993fe8d705e5fff12ba18c
Kusota <93075303+Kusota@users.noreply.github.com>
2022-10-21 11:15:39 +1000

    pet game

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
 from microbit import *
-import radio
+import audio
 
-radio.on()
+timer = 0
+display.show(Image(
+    "00000:"
+    "09090:"
+    "00000:"
+    "09990:"
+    "00000"))
+audio.play(Sound.HELLO)
+
 while True:
-    try:
-        radio.send_bytes("x"+str(accelerometer.get_x()))
-        radio.send_bytes("y"+str(accelerometer.get_y()))
-        display.show(Image.ANGRY)
-    except:
-        display.scroll("FAIL")
-        radio.send_bytes("FAIL")
+    if pin_logo.is_touched():
+        timer = 0
+        display.show(Image.HAPPY)
+    elif accelerometer.was_gesture('shake'):
+        timer = 0
+        display.show(Image.SURPRISED)
+        
+    else:
+        sleep(500)
+        timer += 0.5
+        # sleep for half a second only to make it react more quickly to logo touch & shake
+        
+    if timer == 20:
+        display.show(Image.SAD)
+       
+    elif timer == 30:
+        display.show(Image.ASLEEP)
+    
+    elif timer == 40:
+        display.show(Image.SKULL)
+       
+        break
