fd688a6a5c000b6be0f5d82ef6b0571d4f101fed
banna12345bob <59811283+banna12345bob@users.noreply.github.com>
2022-10-26 09:56:18 +1000

    Scuffed as microbit inputs

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from microbit import *
 import radio
 
 radio.on()
+player = 2
 while True:
     try:
-        radio.send_bytes("x"+str(accelerometer.get_x()))
-        radio.send_bytes("y"+str(accelerometer.get_y()))
+        # radio.send_bytes("x"+str(accelerometer.get_x()))
+        radio.send_bytes(str(player)+"y"+str(pin0.read_analog() - 495))
         display.show(Image.ANGRY)
     except:
         display.scroll("FAIL")
--- receiver.py
+++ receiver.py
@@ -1,7 +1,11 @@
-# from bitio.src import microbit
+from bitio.src import microbit
+from multiprocessing import Process
 import math
 import pygame
+
+
 # https://gamedev.stackexchange.com/questions/4253/in-pong-how-do-you-calculate-the-balls-direction-when-it-bounces-off-the-paddl
+# https://stackoverflow.com/questions/29640685/how-do-i-detect-collision-in-pygame
 pygame.init()
 screen = pygame.display.set_mode(flags=pygame.FULLSCREEN)
 clock = pygame.time.Clock()
@@ -9,17 +13,18 @@ clock = pygame.time.Clock()
 # Displays text that says hello in green
 font = pygame.font.SysFont(None, 24)
 
+microbit.radio.on()
 def update_fps():
 	fps = str(int(clock.get_fps()))
 	fps_text = font.render(fps, 1, (255, 0, 0))
 	return fps_text
 
-# microbit.radio.on()
+sensitivity, speed = 5, 5
+yIn1 = yIn2 = 0
 yOne = yTwo = screen.get_height() / 2 - 100
 x, y = screen.get_width() / 2 - 20, screen.get_height() / 2 - 20
-sensitivity, speed = 1, 5
 running, flip, yFlip = True, False, False
-bounceAngle = 0
+bounceAngle = bounceAngleY = 0
 while running:
     yOne = max(0, min(yOne, screen.get_height() - 200))
     yTwo = max(0, min(yTwo, screen.get_height() - 200))
@@ -27,10 +32,7 @@ while running:
     player1 = pygame.Rect(screen.get_width() - 100, yOne, 25, 200)
     player2 = pygame.Rect(100, yTwo, 25, 200)
     ball = pygame.Rect(x, y, 20, 20)
-    # if y >= 0:
-    #     y *= -1
-    # elif y >= screen.get_height():
-    #     y *= -1
+
     colide1 = ball.colliderect(player1)
     colide2 = ball.colliderect(player2)
     if colide1:
@@ -38,16 +40,17 @@ while running:
         relativeIntersectY = (yOne+(200/2)) - intersectY
         normalizedRelativeIntersectionY = (relativeIntersectY/(200/2))
         bounceAngle = normalizedRelativeIntersectionY * 75
+        bounceAngleY = bounceAngle
         flip = True
     elif colide2:
         intersectY = y - ((x - (yOne + 200)) * y) / x
         relativeIntersectY = (yOne+(200/2)) - intersectY
         normalizedRelativeIntersectionY = (relativeIntersectY/(200/2))
         bounceAngle = normalizedRelativeIntersectionY * 75
+        bounceAngleY = bounceAngle
         flip = False
 
     if not flip:
-        # x += 2 * speed
         if yFlip:
             # instead of this set a flag saying that you hit the edge something like yFlip
             x += speed*math.cos(bounceAngle)
@@ -55,7 +58,7 @@ while running:
             yFlip = False
         else:
             x += speed*math.cos(bounceAngle)
-            y += speed*math.sin(bounceAngle)
+            y += speed*math.sin(bounceAngleY)
     elif flip:
         # x -= 2 * speed
         if yFlip:
@@ -64,41 +67,37 @@ while running:
             yFlip = False
         else:
             x -= speed*math.cos(bounceAngle)
-            y += speed*math.sin(bounceAngle)
-    if y <= 0:
-        yFlip = True
+            y += speed*math.sin(bounceAngleY)
+    if y <= 0 or y >= screen.get_height() - 20:
+        bounceAngleY *= -1
 
-    # incoming = microbit.radio.receive_bytes()
-    # incoming = incoming[2:-1]
-    # if incoming[0] == "x":
-    #     x = int(incoming[1:len(incoming)])
-    #     if x<0.0:
-    #         x *= -1
-    # elif incoming[0] == "y":
-    #     y = int(incoming[1:len(incoming)])
-    #     if y<0.0:
-    #         y *= -1
-    # print("x: "+str(x))
-    # print("y: "+str(y))
+    incoming = microbit.radio.receive_bytes()
+    incoming = incoming[2:-1]
+    if incoming[0] == "1":
+        if incoming[1] == "y":
+            yIn1 = int(incoming[2:len(incoming)])
+            yTwo += yIn1 * 0.025 * sensitivity
+    elif incoming[0] == "2":
+        if incoming[1] == "y":
+            yIn2 = int(incoming[2:len(incoming)])-20
+            yOne += yIn2 * 0.025 * sensitivity
+
+    if x <= 0 or x >= screen.get_width():
+        x, y = screen.get_width() / 2 - 20, screen.get_height() / 2 - 20
+        yOne = yTwo = screen.get_height() / 2 - 100
 
     # Pygame stuff
     # Events
     keyState = pygame.key.get_pressed()
-    # if keyState[pygame.K_d]:
-    #     x += 1
-    #     print('d')
-    # if keyState[pygame.K_a]:
-    #     x -= 1
-    #     print('a')
     # Very icky ties movement speed to FPS. Fixed by limiting FPS to 60
-    if keyState[pygame.K_DOWN]:
-        yOne += 10 * sensitivity
-    if keyState[pygame.K_UP]:
-        yOne -= 10 * sensitivity
-    if keyState[pygame.K_w]:
-        yTwo -= 10 * sensitivity
-    if keyState[pygame.K_s]:
-        yTwo += 10 * sensitivity
+    # if keyState[pygame.K_DOWN]:
+    #     yOne += 10 * sensitivity
+    # if keyState[pygame.K_UP]:
+    #     yOne -= 10 * sensitivity
+    # if keyState[pygame.K_w]:
+    #     yTwo -= 10 * sensitivity
+    # if keyState[pygame.K_s]:
+    #     yTwo += 10 * sensitivity
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
